634807ed421ff41594d8c0728a259bdf43406a15
arnaudbain <arnaudbain@gmail.com>
2026-04-27 18:26:19 +0200

    Add RootLayout component with metadata and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
 
+import "./globals.css";
+
+export const metadata = {
+  title: "Boutique Mihi - Arnaud Bain",
+  description: "Partenaire Indépendant Mihi",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
